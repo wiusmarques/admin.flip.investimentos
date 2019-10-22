954f9eed80350b58841b91c055b1c98809af48be
Wiusmarques Moreira dos Reis <wiusmarques.dev@outlook.com>
2019-10-21 22:56:53 -0300

    Upload themes

--- plugins/siapp/website/Plugin.php
+++ plugins/siapp/website/Plugin.php
@@ -1,15 +1,17 @@
 <?php namespace siapp\Website;
+require 'vendor/autoload.php';
 
 use System\Classes\PluginBase;
 use RainLab\User\Models\Settings as UserSettings;
 use siapp\Website\Models\ActivationCode;
-use siapp\Website\Classes\Mail;
+
 use Route;
 use Auth;
 use Event;
 use Validator;
 use Request;
 use Response;
+use siapp\Register\Models\EmailProvider;
 
 class Plugin extends PluginBase
 {
@@ -61,16 +63,18 @@ class Plugin extends PluginBase
                     $errors = $this->validateData($data, $rules);
                     
                     if($errors != ""){
-                        return $errors;
+                        return Response::make($errors, 403);
                     }
                     
                     $user = Auth::findUserByLogin($data['email']);
 
                     if($user){
-                        return [
+                        $errors = [
                             "Type" => "Erro",
                             "Data" => "Esta conta já está sendo utilizada.",
                         ];
+
+                        return Response::make($errors, 403);
                     }
 
                     
@@ -80,15 +84,48 @@ class Plugin extends PluginBase
                         'password' => $data['password'],
                         'password_confirmation' => $data['password_confirmation'],
                     ]);
+
+
+
+                    if($user){
+
+                        $code = md5($user->mail . date("Y-m-d H:i:s"));
+                        $html = file_get_contents("http://localhost/flip/email/confirmation/" . $user->name);
+                        trace_log($html);
+                        $email = new \SendGrid\Mail\Mail(); 
+                        $email->setFrom("noreply@flipinvestimentos.com", "Flip Invistimentos");
+                        $email->setSubject("Seja bem vindo! Veja nosso e-mail com o código de verificação");
+                        $email->addTo($user->email, $user->name);
+                        $email->addContent("text/plain", "Seja bem vindo Fliper, clique no link abaixo para validar sua conta de e-mail. ");
+                        $email->addContent("text/html", $html);
+
+                        $data = EmailProvider::select('key')->where("active", 1)->first();
+                        trace_log($data);
+                        //return $key;
+                        $sendgrid = new \SendGrid($data->key);
+
+                        try {
+                            $sendgrid->send($email);
+                            return ['status' => 'success', 'message' => 'O código foi enviado para ' . $user->email];
+                        } catch (Exception $e) {
+                            trace_log("Erro ao tentar enviar o e-mail para: " . $user->email . " entre em contato com o administrador do sistema. \nMensagem de Erro: " . $e->getMessage());
+                        }
+                    }else{
+                        return ['status' => 'success', 'message' => 'O código foi enviado para ' . $user->email];
+                    }
+
                     
-                    return $user;
                 });
+
+
                 
                 Route::post('update', function () {
         
         
                     //return post();
                 });
+
+
         
                 
             });

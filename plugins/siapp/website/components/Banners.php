b7b725750eab3eb1a0d23df513a2fdda13b0c15b
Wiusmarques Moreira dos Reis <wiusmarques.dev@outlook.com>
2019-11-11 23:46:49 -0300

    Adjustment

---
+++ plugins/siapp/website/components/Banners.php
@@ -0,0 +1,33 @@
+<?php namespace Siapp\Website\Components;
+
+use siapp\Website\Models\Banner;
+
+class Banners extends ComponentBase
+{
+    public $banners;
+
+
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Banners',
+            'description' => 'Permite exibir uma listagem de banners do site',
+        ];
+    }
+
+    public function onRun()
+    {
+
+        $this->banners = Banner::all();
+        
+        if ($this->property('bannerLocation')) {
+            $locationID = $this->property('bannerLocation');
+            $this->banners = Banner::where('location', $locationID);
+        }
+
+        
+    }
+
+}
+
+?>

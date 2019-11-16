6bc36ec9e6a1e8afbff9fc0846d33d1d4fc37291
Wiusmarques Moreira dos Reis <wiusmarques.dev@outlook.com>
2019-11-16 15:46:22 -0300

    Adjustment

--- plugins/siapp/website/controllers/Content.php
+++ plugins/siapp/website/controllers/Content.php
@@ -55,6 +55,6 @@ class Content extends Controller
             $section = $sectionSelected;
         }
 
-        return $query->Where('section', $section);
+        return $query->Where('section', $section)->orderBy('id', 'desc');
     }
 }
